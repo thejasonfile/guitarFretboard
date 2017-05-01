1d817b5b6f9bca7e62c70005eddae498bb62a2c1
Jason Arnold <jason.arnold.00@gmail.com>
2017-05-01 15:03:50 -0400

    added all minor key objects

--- js/script.js
+++ js/script.js
@@ -1,19 +1,57 @@
-var scales = {
-    // 'aminor' : ['a', 'c', 'd', 'e', 'g'],
-    // 'bminor' : ['b', 'd', 'e', 'fsharp', 'a']
-    'aminor' : 5,
-    'bminor' : 7,
-    'cminor' : 8,
-    'dminor' : 10,
-    'eminor' : 12,
-    'fminor' : 13,
-    'gminor' : 15
+var positionOne = {
+        'frets': [0, 3, 18, 21, 36, 38, 54, 56, 72, 74, 90, 93],
+        'aminor' : [5],
+        'bminor' : [7],
+        'cminor' : [8],
+        'dminor' : [10],
+        'eminor' : [0, 12],
+        'fminor' : [1, 13],
+        'gminor' : [3]
 }
-var positionOne = [0, 3, 18, 21, 36, 38, 54, 56, 72, 74, 90, 93];
-var positionTwo = [3, 5, 21, 23, 38, 40, 56, 59, 74, 77, 93, 95];
-var positionThree = [5, 7, 23, 26, 40, 43, 59, 61, 77, 79, 95, 97];
-var positionFour = [-5, -2, 7, 10, 14, 16, 26, 28, 31, 33, 43, 45, 49, 51, 61, 63, 67, 70, 79, 82, 85, 88, 97, 100];
-var positionFive = [-2, 0, 10, 12, 16, 18, 28, 30, 33, 36, 45, 48, 51, 54, 63, 66, 70, 72, 82, 84, 88, 90, 100, 102];
+
+var positionTwo = {
+        'frets': [0, 2, 18, 20, 35, 37, 53, 56, 71, 74, 90, 92],
+        'aminor' : [8],
+        'bminor' : [10],
+        'cminor' : [11],
+        'dminor' : [1, 13],
+        'eminor' : [3, 15],
+        'fminor' : [4],
+        'gminor' : [6]
+}
+
+var positionThree = {
+        'frets': [0, 2, 18, 21, 35, 38, 54, 56, 72, 74, 90, 92],
+        'aminor' : [10],
+        'bminor' : [12],
+        'cminor' : [1, 13],
+        'dminor' : [3],
+        'eminor' : [5],
+        'fminor' : [6],
+        'gminor' : [8]
+}
+
+var positionFour = {
+        'frets': [0, 3, 19, 21, 36, 38, 54, 56, 72, 75, 90, 93],
+        'aminor' : [0, 12],
+        'bminor' : [2, 14],
+        'cminor' : [3],
+        'dminor' : [5],
+        'eminor' : [7],
+        'fminor' : [8],
+        'gminor' : [10]
+}
+
+var positionFive = {
+        'frets': [0, 2, 18, 20, 35, 38, 53, 56, 72, 74, 90, 92],
+        'aminor' : [3, 15],
+        'bminor' : [5],
+        'cminor' : [6],
+        'dminor' : [8],
+        'eminor' : [10],
+        'fminor' : [11],
+        'gminor' : [1, 13]
+}
 
 $(() => {
     var allFrets = $('.fret');
@@ -21,45 +59,41 @@ $(() => {
     $('#scale-select').add('#position-select').add('#key-select').change(() => {
         var selectedScale = $('#scale-select').val();
         var selectedKey = $('#key-select').val();
+        var key = `${selectedScale}${selectedKey}`
         var selectedPosition = $('#position-select').val();
-        var rootNote = scales[`${selectedScale}${selectedKey}`];
-        var frets = [];
-        // var rootFret = $(`#six-string > .${selectedScale}`);
-        $('.fret').removeClass('selected');
 
-        if (selectedPosition === 'one') {
-            positionOne.forEach((fret) => {
-             	frets.push(allFrets[fret + rootNote])
+        var removeHighlights = () => {
+            allFrets.removeClass('selected');
+        }
+
+        var highlightFrets = (position) => {
+            var rootNotes = position[key]
+            rootNotes.forEach((rootNote) => {
+                position['frets'].forEach((fret) => {
+                    allFrets[rootNote+fret].className += (' selected');
+                })
             })
         }
 
-        if (selectedPosition === 'two') {
-            positionTwo.forEach((fret) => {
-             	frets.push(allFrets[fret + rootNote])
-            })
-        }
+        removeHighlights();
 
-        if (selectedPosition === 'three') {
-            positionThree.forEach((fret) => {
-             	frets.push(allFrets[fret + rootNote])
-            })
+        switch (selectedPosition) {
+            case 'one':
+                highlightFrets(positionOne);
+                break;
+            case 'two':
+                highlightFrets(positionTwo);
+                break;
+            case 'three':
+                highlightFrets(positionThree);
+                break;
+            case 'four':
+                highlightFrets(positionFour);
+                break;
+            case 'five':
+                highlightFrets(positionFive);
+                break;
         }
-
-        if (selectedPosition === 'four') {
-            positionFour.forEach((fret) => {
-             	frets.push(allFrets[fret + rootNote])
-            })
-        }
-
-        if (selectedPosition === 'five') {
-            positionFive.forEach((fret) => {
-             	frets.push(allFrets[fret + rootNote])
-            })
-        }
-
-        frets.forEach((fret) => {
-            fret.className += ' selected';
-        })
     })
 
 })
